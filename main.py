d37d39e4788e41575343c497d5c650d5b9365777
Christopher Lindenberg <chris@AirvonCistopher.fritz.box>
2024-11-02 18:08:58 +0100

    Changes default game in main.py from CarRacing-v3 to LunarLander-v3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from configs import get_config
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--game", "-g", type=str, default="CarRacing")
+parser.add_argument("--game", "-g", type=str, default="LunarLander")
 args = parser.parse_args()
 
 env_fn, agent, trainer_kwargs, train_steps = get_config(args.game)
